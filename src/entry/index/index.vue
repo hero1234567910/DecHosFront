cff9d40484bef6c721b132b6658e214b538bc521
hero12345679 <1039323578@qq.com>
2019-06-11 09:07:26 +0800

    提交文件夹

--- src/entry/index/index.vue
+++ src/entry/index/index.vue
@@ -1,17 +1,6 @@
 <template>
   <div id="bac">
-    <div class="weui-flex">
-		  <div class="weui-flex__item">weui</div>
-		  <div class="weui-flex__item" @click.prevent="ceshi">weui</div>
-		</div>
-		<a href="javascript:;" class="weui-btn weui-btn_primary">按钮</a>
-<a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_primary">按钮</a>
-<a href="javascript:;" class="weui-btn weui-btn_warn">确认</a>
-<a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_warn">确认</a>
-<a href="javascript:;" class="weui-btn weui-btn_default">按钮</a>
-<a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_default">按钮</a>
-<a href="javascript:;" class="weui-btn weui-btn_plain-default">按钮</a>
-<a href="javascript:;" class="weui-btn weui-btn_plain-primary">按钮</a>
+ 		
   </div>
 </template>
 
@@ -21,9 +10,7 @@
 import weui from 'jquery-weui/dist/js/jquery-weui.min'
   export default {
   	methods:{
-  		ceshi(){
-  			$.alert("法法")
-  		}
+  		
   	}
   }
   </script>
