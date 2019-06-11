e1b254e2a73b53feae857ab2023fe347918be4d2
hero12345679 <1039323578@qq.com>
2019-06-11 08:55:53 +0800

    引入样式文件

--- src/entry/index/index.vue
+++ src/entry/index/index.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div id="bac">
+    <div class="weui-flex">
+		  <div class="weui-flex__item">weui</div>
+		  <div class="weui-flex__item" @click.prevent="ceshi">weui</div>
+		</div>
+		<a href="javascript:;" class="weui-btn weui-btn_primary">按钮</a>
+<a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_primary">按钮</a>
+<a href="javascript:;" class="weui-btn weui-btn_warn">确认</a>
+<a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_warn">确认</a>
+<a href="javascript:;" class="weui-btn weui-btn_default">按钮</a>
+<a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_default">按钮</a>
+<a href="javascript:;" class="weui-btn weui-btn_plain-default">按钮</a>
+<a href="javascript:;" class="weui-btn weui-btn_plain-primary">按钮</a>
   </div>
 </template>
 
+
 <script>
+	
+import weui from 'jquery-weui/dist/js/jquery-weui.min'
   export default {
+  	methods:{
+  		ceshi(){
+  			$.alert("法法")
+  		}
+  	}
   }
-</script>
+  </script>
 
 <style>
+	#bac{
+		background-color: aqua;
+	}
 </style>
