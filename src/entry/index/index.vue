d77f7ac4d118e67d685ab9df2cdc299a272ba22d
hero12345679 <1039323578@qq.com>
2019-06-13 17:13:06 +0800

    提交

--- src/entry/index/index.vue
+++ src/entry/index/index.vue
@@ -1,130 +1,30 @@
 <template>
-  <div>
- 		<div class="hero-banner">
- 			<img src="../../../static/img/图层-646-拷贝.png" class="hero-img" width="100%"/>
- 		</div>
- 		
- 		<div class="hero-search">
-	 		<div class="weui-search-bar" id="searchBar">
-			  <form class="weui-search-bar__form">
-			    <div class="weui-search-bar__box">
-			      <i class="weui-icon-search"></i>
-			      <input type="search" class="weui-search-bar__input" id="searchInput" placeholder="" required="">
-			      <a href="javascript:" class="weui-icon-clear" id="searchClear"></a>
-			    </div>
-			    <label class="weui-search-bar__label" id="searchText">
-			      <i class="weui-icon-search"></i>
-			      <span>搜索</span>
-			    </label>
-			  </form>
-			  <a href="javascript:" class="weui-search-bar__cancel-btn" id="searchCancel">取消</a>
-			</div>
-		</div>
+  <div style="height: 100%;">
+ 		<div class="weui-tab">
+		  <div class="weui-tab__bd" style="height: 92%;position: absolute;width: 100%;">
+        <div id="tab1" class="weui-tab__bd-item weui-tab__bd-item--active weui-iframe" style="overflow: hidden;">
+          <home></home>
+        </div>
+        <div id="tab2" class="weui-tab__bd-item weui-iframe">
+          
+        </div>
+      </div>
 		
-		<div class="hero-panel">
-			<div class="hero-panel2">
-				<div class="hero-panel2_header"><strong>门诊服务</strong></div>
-				<div style="margin-left: 5px;margin-right: 5px;width: calc(100% - 10px);">
-					<div class="weui-row">
-					  <div class="weui-col-33"> <!--style="background-image:url(../../../static/img/预约.png)	; background-size: 100% 100%;"-->
-					  		<img src="../../../static/img/预约.png" style="width: 100%;"/>
-					  </div>
-					  <div class="weui-col-33">
-					  	<img src="../../../static/img/缴费.png" style="width: 100%;"/>
-					  </div>
-					  <div class="weui-col-33">
-					  	<img src="../../../static/img/健康卡1.png" style="width: 100%;" />
-					  </div>
-					</div>
-					<div style="margin-left: 20px;margin-right: 20px;margin-top: 10px;height: 60px;">
-						<div class="weui-row" style="height: 80px;">
-							<div class="weui-col-25">
-								<div class="hero-col-img">
-									<img src="../../../static/img/导诊台.png" style="width: 100%;"/>
-								</div>
-								<div class="hero-col-dec" style="min-width: 70px;">导诊台确认</div>
-							</div>
-							<div class="weui-col-25">
-								<div class="hero-col-img">
-									<img src="../../../static/img/咨询图标.png" style="width: 100%;"/>
-								</div>
-								<div class="hero-col-dec">门诊咨询</div>
-							</div>
-							<div class="weui-col-25">
-								<div class="hero-col-img">
-									<img src="../../../static/img/图层-579.png" style="width: 100%;"/>
-								</div>
-								<div class="hero-col-dec">就诊需知</div>
-							</div>
-							<div class="weui-col-25">
-								<div class="hero-col-img">
-									<img src="../../../static/img/复诊提醒1.png" style="width: 100%;"/>
-								</div>
-								<div class="hero-col-dec">复诊提醒</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div style="margin-top: 160px;margin-left: 13px;margin-right: 13px;width: calc(100% - 26px);">
-			<div class="hero-panel2_header" style="height: 30px;"><strong>在院服务</strong></div>
-			<div style="margin-left: 20px;margin-right: 20px;height: 70px;">
-				<div class="weui-row" style="height: 80px;">
-					<div class="weui-col-25">
-						<div class="hero-col-img">
-							<img src="../../../static/img/科室预约-.png" style="width: 100%;"/>
-						</div>
-						<div class="hero-col-dec" style="min-width: 70px;">科室预约</div>
-					</div>
-					<div class="weui-col-25">
-						<div class="hero-col-img">
-							<img src="../../../static/img/病房预约-.png" style="width: 100%;"/>
-						</div>
-						<div class="hero-col-dec">病房预约</div>
-					</div>
-					<div class="weui-col-25">
-						<div class="hero-col-img">
-							<img src="../../../static/img/入院导引.png" style="width: 100%;"/>
-						</div>
-						<div class="hero-col-dec">入院导引</div>
-					</div>
-					<div class="weui-col-25">
-						<div class="hero-col-img">
-							<img src="../../../static/img/住院须知.png" style="width: 100%;"/>
-						</div>
-						<div class="hero-col-dec">住院须知</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-		<div style="height: 10px;width: 100%;margin-top: 15px;background-color: #e5e5e5;"></div>
-		
-		<div class="hero-panel3">
-			<div class="weui-flex">
-			  <div class="weui-flex__item">
-			  	<div class="hero-item-img">
-							<img src="../../../static/img/住院中--.png" style="width: 100%;"/>
-							<div class="hero-mButton">
-								<img src="../../../static/img/进入.png" style="width: 100%;"/>
-							</div>
-						</div>
-			  </div>
-			  <div class="weui-flex__item">
-			  	<div class="hero-item-img" style="margin-top: 20px">
-							<img src="../../../static/img/图片1.png" style="width: 100%;"/>
-							<div class="hero-mButton" style="top: 56px;">
-								<img src="../../../static/img/进入.png" style="width: 100%;"/>
-							</div>
-						</div>
-			  </div>
-			</div>
-		</div>
-		
-		<div style="height: 10px;width: 100%;margin-top:8px;background-color: #e5e5e5;"></div>
-		<div style="width: 100%;height: 100px;">
-			<img src="../../../static/img/体检.png" style="width: 100%;"/>
+		  <div class="weui-tabbar">
+		    <a href="#tab1" class="weui-tabbar__item weui-bar__item--on">
+		      <!--<span class="weui-badge" style="position: absolute;top: -.4em;right: 1em;">8</span>-->
+		      <div class="weui-tabbar__icon">
+		        <img src="../../../static/img/icon_nav_button.png" alt="">
+		      </div>
+		      <p class="weui-tabbar__label">主页</p>
+		    </a>
+		    <a href="#tab2" class="weui-tabbar__item">
+		      <div class="weui-tabbar__icon">
+		        <img src="../../../static/img/icon_nav_cell.png" alt="">
+		      </div>
+		      <p class="weui-tabbar__label">个人中心</p>
+		    </a>
+		  </div>
 		</div>
   </div>
 </template>
@@ -133,7 +33,9 @@
 <script>
 	
 import weui from 'jquery-weui/dist/js/jquery-weui.min'
+import home from './components/home'
   export default {
+  	components:{home},
   	methods:{
   		
   	}
