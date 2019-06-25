aaaa9a2d47f8c5cbded76c2c95771f9e86f30729
wzlggg <wzl1888@126.com>
2019-06-25 16:58:02 +0800

    修改

--- src/entry/hosprofile/hosProfile.vue
+++ src/entry/hosprofile/hosProfile.vue
@@ -1,68 +1,330 @@
 <template>
-  <div >
-    <div class="weui-tab" id="page-infinite-navbar" >
+  <div>
+    <div class="weui-tab" id="page-infinite-navbar">
       <div class="weui-tab__bd" id="#app">
         <div id="tab1" class="weui-tab__bd-item weui-tab__bd-item--active">
           <h2 class="doc-head">{{title}}</h2>
           <h4 class="doc-info">发布时间:{{infoDate}}</h4>
           <h6></h6>
-          <div class="hos-content" v-html="content">
-            {{content}}
-          </div>
-          </div>
+          <div class="hos-content" v-html="content">{{content}}</div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 import weui from "jquery-weui/dist/js/jquery-weui.min";
 //import evn from '../index/utils/evn.js';
-import model from './model.js'
+import model from "./model.js";
 export default {
   name: "hosProfile.vue",
-  data(){
-    this.model = model(this.axios)
-    
-    return{
-      title:'',
-      infoDate:'',
-      createUserName:'',
-      content:''
-    }
+  data() {
+    this.model = model(this.axios);
+
+    return {
+      title: "",
+      infoDate: "",
+      createUserName: "",
+      content: ""
+    };
   },
-  mounted(){
-    this.getHosIntroduction();
+  created(){
+    this.judge();
+    //this.GetQueryString();
+  },
+  mounted() {
+    //this.judge();
   },
   methods: {
-    getHosIntroduction(){
+    //修改过的
+    GetQueryString(name) {
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+      var r = location.search.substr(1).match(reg); //search,查询？后面的参数，并匹配正则
+      if (r != null) return decodeURI(r[2]);
+      return null;
+    },
+
+    judge() {
       let self = this;
-      let data = this;
-      this.model.getHosIntroduction(data).then(function(res){
-        if(res.data.code == '0'){
-          //console.log(res.data);
+      var infoType = this.GetQueryString("infoType");
+      //console.log(infoType);
+      
+      if (infoType == "HosIntroduction") {
+        //let self = this;
+        let data = this;
+        this.model.getHosIntroduction(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
           //console.log(res.data.data);
-          let title = res.data.data.title;
-          let infoDate = res.data.data.infoDate;
-          let createUserName = res.data.data.createUserName;
-          let content = res.data.data.content;
-          self.title = title;
-          self.infoDate = infoDate;
-          self.createUserName = createUserName;
-          var _str = res.data.data.content.replace(/<img src="/g, '<img class="hos-img" src="http://localhost:9091');
-          //console.log(_str);
-          self.content = _str;
-        }else{
-          $.alert(res.data.msg);
-        }
-        console.log(res.data.data);
-      })
-    }
+        });
+      }
+
+      if(infoType == "OutpatientProcess"){
+        //let self = this;
+        let data = this;
+        this.model.getOutpatientProcess(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "FloorDistribution"){
+        //let self = this;
+        let data = this;
+        this.model.getFloorDistribution(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "PatientNeedtoKnow"){
+        //let self = this;
+        let data = this;
+        this.model.getPatientNeedtoKnow(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "DirectAdmission"){
+        //let self = this;
+        let data = this;
+        this.model.getDirectAdmission(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "Hospitalisation"){
+        //let self = this;
+        let data = this;
+        this.model.getHospitalisation(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "OutHospitalInfo"){
+        //let self = this;
+        let data = this;
+        this.model.getOutHospitalInfo(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "OutNeedKnow"){
+        //let self = this;
+        let data = this;
+        this.model.getOutNeedKnow(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "DepartmentsIntroduction"){
+        //let self = this;
+        let data = this;
+        this.model.getDepartmentsIntroduction(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+
+      if(infoType == "HealthEducation"){
+        //let self = this;
+        let data = this;
+        this.model.getHealthEducation(data).then(function(res) {
+          if (res.data.code == "0") {
+            //console.log(res.data);
+            //console.log(res.data.data);
+            let title = res.data.data.title;
+            let infoDate = res.data.data.infoDate;
+            let createUserName = res.data.data.createUserName;
+            let content = res.data.data.content;
+            self.title = title;
+            self.infoDate = infoDate;
+            self.createUserName = createUserName;
+            var _str = res.data.data.content.replace(
+              /<img src="/g,
+              '<img class="hos-img" src="http://localhost:9091'
+            );
+            //console.log(_str);
+            self.content = _str;
+          } else {
+            $.alert(res.data.msg);
+          }
+          //console.log(res.data.data);
+        });
+      }
+    },
+
+
+    
   }
 };
-
-
-
 </script>
 
 <style>
@@ -77,15 +339,15 @@ export default {
   margin-right: 20px;
   text-indent: 2em;
 }
-body{
-    overflow: auto;
+body {
+  overflow: auto;
 }
-.hos-content{
+.hos-content {
   margin-top: 0px;
   margin-right: 12px;
   margin-left: 12px;
 }
-.hos-img{
+.hos-img {
   display: inline-block;
   height: auto;
   max-width: 100%;
--- src/entry/hosprofile/model.js
+++ src/entry/hosprofile/model.js
@@ -3,7 +3,7 @@ import evn from '../index/utils/evn.js';
 function model(http) {
 
   return {
-    // 获取用户信息
+    // 获取医院简介信息
     getHosIntroduction(data) {
     	//console.log(data);
         return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getHosIntroduction',data,{
@@ -12,6 +12,7 @@ function model(http) {
             }
         });
     },
+    //获取门诊流程
     getOutpatientProcess(data){
         return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getOutpatientProcess',data,{
             headers:{
@@ -19,8 +20,65 @@ function model(http) {
             }
         });
     },
+    //获取楼层分布
     getFloorDistribution(data){
         return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getFloorDistribution',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取就诊须知
+    getPatientNeedtoKnow(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getPatientNeedtoKnow',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取入院引导
+    getDirectAdmission(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getDirectAdmission',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取住院须知
+    getHospitalisation(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getHospitalisation',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取出院导引
+    getOutHospitalInfo(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getOutHospitalInfo',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取出院须知信息
+    getOutNeedKnow(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getOutNeedKnow',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取科室简介
+    getDepartmentsIntroduction(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getDepartmentsIntroduction',data,{
+            headers:{
+                contentType:'application/json;charset=utf-8'
+            }
+        });
+    },
+    //获取健康教育
+    getHealthEducation(data){
+        return http.get(evn.SEC_HOSAPI+'/wx/sys/informationinfo/getHealthEducation',data,{
             headers:{
                 contentType:'application/json;charset=utf-8'
             }
