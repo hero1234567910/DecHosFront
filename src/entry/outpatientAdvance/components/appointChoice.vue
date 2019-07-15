510349522eedd207e58f3a167663f47bf7e77473
hero12345679 <1039323578@qq.com>
2019-07-15 10:27:19 +0800

    选择页面

--- src/entry/outpatientAdvance/components/appointChoice.vue
+++ src/entry/outpatientAdvance/components/appointChoice.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Choice-wzl">
-    <div class="swiper-head">
+    <!--<div class="swiper-head">
       <div class="swiper-container">
         <div class="swiper-wrapper">
           <div class="swiper-slide">
@@ -15,7 +15,23 @@
         </div>
         <div class="swiper-pagination"></div>
       </div>
-    </div>
+    </div>-->
+    
+    <div class="ad-header">
+  		<div class="ad-headerImg">
+  			<img src="../../../../static/img/预约 (1).svg" style="width: 11%;position: absolute;top: 7px;
+    left: 17px;"/>
+  		</div>
+  		<div class="ad-title">
+  			<p style="font-size: 20px;font-weight: 700;">门诊在线预约</p>
+  		</div>
+  		<!--<div class="ad-fin">
+  			<img src="../../../../static/img/在院.png" style="position: absolute;width: 73%;
+    top: 20px;
+    left: 100px;"/>
+  		</div>-->
+  	</div>
+    
     <div class="img-content">
       <img style="margin-top: 10px;margin-left: 34%;" src="../../../../static/images/当天门诊.png" />
     </div>
@@ -34,7 +50,6 @@
 
 <script>
 import Swiper from "swiper";
-import "swiper/dist/css/swiper.min.css";
 import model from "./model.js";
 
 export default {
@@ -71,14 +86,32 @@ export default {
 
 
 <style scoped>
-.swiper-img {
-  width: 100%;
-  align-content: center;
-  height: 175px;
-  border-radius: 6px;
-}
+	.ad-fin{
+		height: 60px;
+		width: 60px;
+		line-height: 60px;
+		position: relative;
+	}
+	.ad-title{
+		float:left;
+		height: 60px;
+		line-height: 60px;
+	}
+	.ad-headerImg{
+		width: 80px;
+		height: 60px;
+		line-height: 60px;
+		float: left;
+	}
+	.ad-header{
+		position: relative;
+		width: 100%;
+		height: 60px;
+		background-color: #FFFFFF;
+	}
 .img-content {
   height: 145px;
+  margin-top: 40px;
 }
 .weui-btn_primary {
   background-color: #4ccbdb;
